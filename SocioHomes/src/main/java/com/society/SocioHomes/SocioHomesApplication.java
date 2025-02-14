22758720ebe9e6d7e410ce9d7ee2e7dcb1231795
SaloniSharma1610 <salonisharma1610@gmail.com>
2025-02-14 13:14:48 +0530

    My backend

---
+++ SocioHomes/src/main/java/com/society/SocioHomes/SocioHomesApplication.java
@@ -0,0 +1,13 @@
+package com.society.SocioHomes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocioHomesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocioHomesApplication.class, args);
+	}
+
+}
---
+++ SocioHomes/src/test/java/com/society/SocioHomes/SocioHomesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.society.SocioHomes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocioHomesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
